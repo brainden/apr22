ef39e79e6c74669edda97fadc28c2353b4f57b3e
Brandon Kilic <brandonkilic@gmail.com>
2022-05-12 23:55:36 -0400

    updated ui

--- discord-clone/src/components/Chat.js
+++ discord-clone/src/components/Chat.js
@@ -1,6 +1,6 @@
 import '../styles/Chat.css'
 import Chatheader from '../components/Chatheader'
-import React from 'react'
+import React, { useState } from 'react'
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
 import GifIcon from "@material-ui/icons/Gif";
@@ -11,6 +11,23 @@ import { Avatar } from '@material-ui/core';
 
 
 function Chat() {
+  const [messageArr, setMessageArr] = useState([]);
+
+  const [messageText, setMessageText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMessage = {
+      message: messageText
+    }
+    addMessage(newMessage);
+    setMessageText('');
+  };
+
+  const addMessage = (messageText) => {
+    setMessageArr([...messageArr, messageText]);
+  };
+
   return (
     <div className='chat'>
       <Chatheader />
@@ -31,12 +48,29 @@ function Chat() {
             </div>
           );
         })}
+        <ul>
+          {messageArr.map(({ id, message }) => (
+            <div className='messages'>
+              <div>
+                <Avatar />
+              </div>
+              <div className='message__info'>
+                <h4 key={id}>Bruce wayne
+                  <span class="message__timestamp">2/22/22</span>
+                </h4>
+                <p>{message}</p>
+              </div>
+            </div>
+          ))}
+        </ul>
       </div>
       <div className='chat__input'>
         <AddCircleIcon fontSize="large" />
 
-        <form>
-          <input placeholder="Message #TESTCHANNEL"></input>
+        <form addMessage={addMessage} onSubmit={handleSubmit}>
+          <input placeholder="Message #TESTCHANNEL" type='text'
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}></input>
           <button className='chat__inputButton' type='submit'>Send Message</button>
         </form>
         <div className='chat__inputIcons'>
